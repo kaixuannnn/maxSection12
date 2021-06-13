4c0ad5823429f54f9290919045a691d84e5bbdd1
Kai Xuan <kaixuan@agmostudio.com>
2021-06-13 18:54:06 +0800

    react action

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # maxSection12
+
+- Every **state Changes**, **Prop changes**, **context changes**, would result in this compoenent fucntion, being executed again
+- for the reactDom, only the affected tag will affected and change
--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Button from './components/UI/Button/Button'
 
-import './App.css';
+import './App.css'
 
 function App() {
+  const [showParagraph, setIsShowParagraph] = useState(false)
+
+  console.log('APP CHANGESSS')
+
+  const toggleParagraphHandler = () => {
+    setIsShowParagraph((prevShowParagraph) => !prevShowParagraph)
+  }
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
